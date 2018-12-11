228ec6e8a44277e4cdd61836a2d19f7f36d45a40
Viraj Chitnis <chitnisviraj@gmail.com>
2018-12-11 21:43:36 +0000

    The prime number calculator times itself

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <csignal>
+#include <time.h>
 #include "Calc.h"
 
 using namespace std;
 
 int pcount = 0;
+clock_t start;
+double duration;
 
 void signalHandler(int signum);
+void showTimerAndCount();
 
 int main() {
   // register signal SIGINT and signal handler
   signal(SIGINT, signalHandler);
+
+  start = clock();
 
   int n = 1;
   while (true) {
@@ -20,12 +26,20 @@ int main() {
     }
     n++;
   }
-  cout << pcount << " prime numbers." << endl;
+  showTimerAndCount();
   return 0;
 }
 
 void signalHandler(int signum) {
   // cout << "Interrupt signal (" << signum << ") received." << endl;
-  cout << endl << pcount << " prime numbers calculated so far." << endl;
+  showTimerAndCount();
   exit(signum);
+}
+
+void showTimerAndCount() {
+  duration = (clock() - start) / (double)CLOCKS_PER_SEC;
+  int hours = duration / 3600;
+  int minutes = (duration - (hours * 3600)) / 60;
+  double seconds = duration - (minutes * 60);
+  cout << endl << pcount << " prime numbers calculated in " << hours << " hours, " << minutes << " minutes, and " << seconds << " seconds." << endl;
 }
